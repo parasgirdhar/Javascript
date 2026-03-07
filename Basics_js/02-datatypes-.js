7d3fb31b2ec12da822d969fe5668ef8dcb1fef25
parasgirdhar <parasy219@gmail.com>
2026-03-07 09:41:33 +0000

    datatypes used in js

---
+++ Basics_js/02-datatypes-.js
@@ -0,0 +1,36 @@
+// alert("paras girdhar")    compiler give error on this beacuse it is the work of browser and we are using it on nodejs environment it is a server  
+// and also if we use it with console.log than also we need to use browser first go to inspect than go in console.. 
+// we mainly use this when we are using index.html file and inject script inside it...
+
+// Here are below some data types which are generally used in js...
+
+let age=28;
+
+let name="paras girdhar";
+
+let isboolean=true;
+
+let nums= [3,6,8,2];
+
+let point=3.2;
+
+let address; //undefined...
+
+let place= null;  
+
+// here all above are datatypes..
+// string => "paras";
+// int / number => 56;
+//  float => 6.3;
+//  null => null is also a data type it means empty not 0..
+// undefined => undefined is 
+
+// simple way to check any type of data of any data...
+
+console.log(typeof 45); // number..
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof false);
