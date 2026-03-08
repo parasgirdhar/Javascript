405ea065ad769681cdcd8c3ea5061231cf7951a6
parasgirdhar <parasy219@gmail.com>
2026-03-08 17:52:32 +0000

    conversion operations

---
+++ Basics_js/04-conversions-operations.js
@@ -0,0 +1,31 @@
+let nums=56;
+
+let numnegative=-nums;
+console.log(numnegative);
+
+let str1="paras";
+let str2=" girdhar";
+
+let str3=str1+str2;
+console.log(str3); // normal string + string...
+
+let a=1+1;
+let b="2";
+let c=a+b;
+console.log(c);  // this is called concatination this is happened when we try to add string into a number it acctualy concatinate both of them...
+
+console.log("1" +2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+
+// prefix or postfix...
+
+let x=10;
+console.log(++x); // value incremented by one ...
+
+let z=11;
+console.log(z++); // here value remain same 11 .. increased next time...
+console.log(z); // here value is 12
