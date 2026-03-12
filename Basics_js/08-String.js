3525c78280b06ab0bac62fcca0e79cf6a85d7148
parasgirdhar <parasy219@gmail.com>
2026-03-12 18:14:15 +0000

    Using of String in js

---
+++ Basics_js/08-String.js
@@ -0,0 +1,16 @@
+let name="paras girdhar";
+
+let age="23";
+
+let repoCount="4";
+
+console.log(`My name is ${name.toUpperCase()}  My age is ${age} And In github my total repositories are ${repoCount}`);
+
+// console.log(`Now my name in lower is ${name.toUpperCase()}`);
+
+let id=new String ("paras-pink"); // professional way to write String ..
+
+console.log(id.charAt(8)); // check character at which position..
+
+const newname= id.substring(0,4);
+console.log(newname);
